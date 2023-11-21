949dd0947cad15ab7e478095aec360344cb8644a
Owen Connors <owenconnors1@gmail.com>
2023-11-21 12:37:58 +0000

    Add alerts using toastify

---
+++ src/graphics/reader/alerts.graphic.tsx
@@ -0,0 +1,26 @@
+import '../uwcs-bootstrap.css';
+import { Notification } from './components/notification';
+import './alerts.graphic.css';
+
+import { createRoot } from 'react-dom/client';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+nodecg.listenFor("show-dono", "nodecg-tiltify", (dono) => {
+    toast(<Notification dono={dono} />, {
+        toastId: dono.id,
+        position: "bottom-left",
+        autoClose: 10000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+    })
+    nodecg.sendMessageToBundle("set-donation-shown", "nodecg-tiltify", [dono, true]);
+})
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<ToastContainer limit={5} closeButton={false} newestOnTop={true} />);
---
+++ src/graphics/reader/components/notification.tsx
@@ -0,0 +1,15 @@
+import { Donation } from "nodecg-tiltify/src/types/schemas/donations";
+import { formatAmounts } from "../utils";
+
+export function Notification({ dono }: { dono: Donation }) {
+    return (
+        <>
+            <h6>
+                <b><i className="bi bi-star-fill"></i> {dono.donor_name}</b>
+                {" donated "}
+                <b>{formatAmounts(dono.amount, dono.displayAmount)}</b>
+            </h6>
+            <div className="msg">{dono.donor_comment}</div>
+        </>
+    )
+}
