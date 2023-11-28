4f0de8ca0cd5d083818d121e92e4fc137cc93ea2
ericthelemur <owenconnors1@gmail.com>
2023-11-28 18:09:47 +0000

    Add announcement bar

--- src/graphics/components/bar.tsx
+++ src/graphics/components/bar.tsx
@@ -1,22 +1,42 @@
 import './bar.scss';
 
 import { Total } from 'nodecg-tiltify/src/types/schemas';
+import { useEffect, useState } from 'react';
+import { ReactFitty } from 'react-fitty';
 import { useReplicant } from 'use-nodecg';
 
 import specialeffect from '../assets/specialeffect-small.png';
 import logo from '../assets/wasd-w.svg';
-import { formatAmount } from '../utils';
+import { formatAmount, timeFormat } from '../utils';
+
+function VR() {
+    return <div className="vr" />;
+}
 
 export function Bar() {
-    const [total, _5] = useReplicant<Total>("total", { "currency": "GBP", "value": 0 }, { namespace: "nodecg-tiltify" });
+    const [total, _] = useReplicant<Total>("total", { "currency": "GBP", "value": 0 }, { namespace: "nodecg-tiltify" });
+    const [announcement, _1] = useReplicant<string>("announcement", 'Donate now at warwickspeed.run/donate');
+
+    const [time, setTime] = useState(timeFormat.format(Date.now()));
+
+    useEffect(() => {
+        const interval = setInterval(() => setTime(timeFormat.format(Date.now())), 10000);
+        return () => clearInterval(interval);
+    }, [setTime]);
 
     return (
         <div className='bar border-top'>
             <img className="logo" src={logo} />
             <h2>WASD 2024</h2>
-            <div className="vr" />
+            <VR />
             <img className="logo" src={specialeffect} />
             <h2>{total && formatAmount(total)}</h2>
+            <VR />
+            <h2 className="announcement h-auto">
+                <ReactFitty minSize={18} maxSize={36} wrapText={true}>{announcement}</ReactFitty>
+            </h2>
+            <VR />
+            <h2>{time}</h2>
         </div>
     )
 }
