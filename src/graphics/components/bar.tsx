d9a52a42d6384f2fbf0899e41435205078902896
ericthelemur <owenconnors1@gmail.com>
2023-11-28 15:21:25 +0000

    Add special effect & total

--- src/graphics/components/bar.tsx
+++ src/graphics/components/bar.tsx
@@ -1,13 +1,22 @@
 import './bar.scss';
 
-import logo2 from '../assets/wasd-w.png';
+import { Total } from 'nodecg-tiltify/src/types/schemas';
+import { useReplicant } from 'use-nodecg';
+
+import specialeffect from '../assets/specialeffect-small.png';
 import logo from '../assets/wasd-w.svg';
+import { formatAmount } from '../utils';
 
 export function Bar() {
+    const [total, _5] = useReplicant<Total>("total", { "currency": "GBP", "value": 0 }, { namespace: "nodecg-tiltify" });
+
     return (
         <div className='bar border-top'>
             <img className="logo" src={logo} />
-            <img className="logo" src={logo2} />
+            <h2>WASD 2024</h2>
+            <div className="vr" />
+            <img className="logo" src={specialeffect} />
+            <h2>{total && formatAmount(total)}</h2>
         </div>
     )
 }
