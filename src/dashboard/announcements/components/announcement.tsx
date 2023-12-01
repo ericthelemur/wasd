200133a27af115bfb55a3100b219ae33f8162a21
ericthelemur <owenconnors1@gmail.com>
2023-12-01 01:37:56 +0000

    Use same editable for priority

--- src/dashboard/announcements/components/announcement.tsx
+++ src/dashboard/announcements/components/announcement.tsx
@@ -1,6 +1,6 @@
 import './announcement.scss';
 
-import { useRef, useState } from 'react';
+import { CSSProperties, EventHandler, useRef, useState } from 'react';
 import { Draggable, DraggableProvided, Droppable } from 'react-beautiful-dnd';
 import {
     CheckLg, GripVertical, Link, PenFill, PlusCircle, Repeat, XCircle, XLg
@@ -26,17 +26,15 @@ export function AnnouncementComp(props: AnnouncementProps) {
     const { announcement, provided } = props;
     return (
         <div ref={provided.innerRef} {...provided.draggableProps} className="card announcement m-1">
-            <div className='card-body hstack gap-2'>
+            <div className={'card-body hstack gap-2' + (announcement.priority === 0 ? " opacity-50" : "")}>
                 <div {...provided.dragHandleProps}>
                     <GripVertical />
                 </div>
-                <Editable text={announcement.text} setText={v => announcement.text = v} ensureUpdate={props.ensureUpdate} />
-                <Form.Control type="number" defaultValue={announcement.priority} style={{ width: "5em" }}
-                    min={0}
-                />
+                <Editable text={announcement.text} setText={v => announcement.text = v} ensureUpdate={props.ensureUpdate} outerStyle={{ flexGrow: 1 }} />
+                <Editable text={announcement.priority.toString()} setText={v => announcement.priority = Number(v)} ensureUpdate={props.ensureUpdate} type="number" />
                 {<Stack direction="horizontal" gap={3}>
-                    <Button onClick={() => {
-                        if (confirm(`Are you sure you want to delete\n"${announcement.text}"? `))
+                    <Button variant="outline-primary" onClick={() => {
+                        if (announcement.priority === 0 || confirm(`Are you sure you want to delete\n"${announcement.text}"? `))
                             props.delete(props.id)
                     }}><XLg /></Button>
                 </Stack>}
@@ -48,29 +46,44 @@ export function AnnouncementComp(props: AnnouncementProps) {
     );
 }
 
+interface EditableProps {
+    text: string;
+    setText: (text: string) => void;
+    ensureUpdate?: () => void;
+    type?: string;
+    outerStyle?: CSSProperties;
+    inputStyle?: CSSProperties;
+}
 
-export function Editable(props: { text: string, setText: (text: string) => void, ensureUpdate?: () => void }) {
+export function Editable(props: EditableProps) {
+    const { text, setText, ensureUpdate, type, outerStyle, inputStyle } = props;
     const [editVal, setEditVal] = useState<string | null>(null);
     const editBox = useRef<HTMLInputElement>(null);
+    const resetEditVal = () => setEditVal(null);
 
     if (editVal === null) {
-        return <span className="editable" onClick={() => setEditVal(props.text)}>{props.text} <PenFill /></span>
+        return <span className="editable" onClick={() => setEditVal(text)} style={outerStyle || {}}>{text} <PenFill /></span>
     } else {
+        const submit = () => {
+            setText(editBox.current!.value);
+            resetEditVal();
+            if (ensureUpdate) ensureUpdate();
+        };
+        const keyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+            if (e.key === "Escape") resetEditVal();
+            else if (["Enter", "Backspace"].includes(e.key)) return;
+            else if (type === "number" && !/[0-9]/.test(e.key)) e.preventDefault();
+        };
+
         return (
-            <Form className="flex-grow-1"
-                onSubmit={() => {
-                    props.setText(editBox.current!.value);
-                    setEditVal(null);
-                    if (props.ensureUpdate) props.ensureUpdate();
-                }}
-            >
+            <Form onSubmit={submit} style={outerStyle || {}}>
                 <InputGroup>
                     <Form.Control ref={editBox} className="editable" autoFocus
-                        defaultValue={editVal}
-                        onKeyDown={e => { if (e.key === "Escape") setEditVal(null) }}
+                        defaultValue={editVal} type={type ? type : "text"}
+                        onKeyDown={keyPress} style={inputStyle || {}}
                     />
                     <Button variant="primary" type="submit"><CheckLg /></Button>
-                    <Button variant="outline-primary" onClick={() => setEditVal(null)}><XLg /></Button>
+                    <Button variant="outline-primary" onClick={resetEditVal}><XLg /></Button>
                 </InputGroup>
             </Form>
         )
@@ -108,7 +121,8 @@ export function AnnPoolComp(props: AnnPoolProps) {
     return (
         <div className="card my-1">
             <div className="card-body">
-                <h3><Editable text={pool.name} setText={(v) => pool.name = v} ensureUpdate={props.ensureUpdate} />
+                <h3 className="m-1">
+                    <Editable text={pool.name} setText={(v) => pool.name = v} ensureUpdate={props.ensureUpdate} />
                 </h3>
                 <div className="position-relative">
                     <div className="addBtn" onClick={() => insertAnnouncement(0)}>
@@ -117,7 +131,7 @@ export function AnnPoolComp(props: AnnPoolProps) {
                 </div>
                 <Droppable droppableId={id}>
                     {(provided) => (
-                        <div className='pool p-1 vstack' {...provided.droppableProps} ref={provided.innerRef}>
+                        <div className='pool vstack' {...provided.droppableProps} ref={provided.innerRef}>
                             {pool.order.map((id, index) => {
                                 const item = pool.announcements[id];
                                 if (item === undefined) return <h3 key={id}>Error: Corresponding Announcement does not exist for announcement id {id}</h3>
