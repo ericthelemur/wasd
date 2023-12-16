73c3b3842b7fd809267f1315e55c0c29495b664a
Owen Connors <owenconnors1@gmail.com>
2023-12-16 18:23:53 +0000

    Refactor out some main components

--- src/dashboard/announcements/announcements.dashboard.tsx
+++ src/dashboard/announcements/announcements.dashboard.tsx
@@ -1,13 +1,13 @@
 import '../../graphics/uwcs-bootstrap.css';
 
-import { DragDropContext, DragStart, DraggableLocation, DropResult } from 'react-beautiful-dnd';
+import { Announce, DragDropContext, DragStart, DraggableLocation, DropResult } from 'react-beautiful-dnd';
 import { Pause, Play, PlusLg, Trash } from 'react-bootstrap-icons';
 import Button from 'react-bootstrap/Button';
 import { createRoot } from 'react-dom/client';
 import { useReplicant } from 'use-nodecg';
 import { AnnBank, AnnPool, AnnPools, AnnQueue, AnnRef, Announcement, CurrentAnnouncement } from '../../types/schemas';
 
-import { useEffect, useState } from 'react';
+import { createRef, useEffect, useState } from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 import { AnnPoolComp } from './components/annpool';
 import { sendTo, sendToF } from 'common/listeners';
@@ -25,6 +25,70 @@ export interface PreludeInfo {
 	lastCA: AnnRef | null;
 	prelude: AnnRef[];
 	length: number;
+}
+
+function sendDragAndDropMove(source: DraggableLocation, destination: DraggableLocation, queue: AnnQueue, pools: AnnPools, preludeLength: number) {
+	if (source.droppableId !== "queue" && destination.droppableId !== "queue") {
+		// Moving pools (pool to pool)
+		if (source.index === destination.index) return;
+		const da = pools![destination.droppableId].announcements;
+		// Adjust for removal of old changing index of drop location
+		const ind = source.droppableId === destination.droppableId && source.index < destination.index ? destination.index + 1 : destination.index;
+		sendTo("movePool", {
+			aref: klona(pools![source.droppableId].announcements[source.index]),
+			oldpid: source.droppableId,
+			newpid: destination.droppableId,
+			before: klona(destination.index === da.length ? null : da[ind])
+		})
+	} else if (source.droppableId === "queue" && destination.droppableId === "queue") {
+		sendTo("reorderQueue", { // Reordering queue elements (queue to queue)
+			aref: queue!.announcements[source.index - preludeLength],
+			before: queue!.announcements[destination.index - preludeLength]
+		});
+	} else if (source.droppableId !== "queue" && destination.droppableId === "queue") {
+		sendTo("enqueue", { // Adding to queue (pool to queue)
+			aid: pools![source.droppableId].announcements[source.index].id,
+			before: queue!.announcements[destination.index - preludeLength]
+		})
+	} else { // (source.droppableId === "queue" && destination.droppableId !== "queue")
+		// Removing from queue (queue to pool)
+		sendTo("dequeue", { aref: queue!.announcements[source.index - preludeLength] })
+	}
+}
+
+function CurrentRun({ currentAnnouncement }: { currentAnnouncement: CurrentAnnouncement }) {
+	return <>
+		<h3>Current</h3>
+		<div className="card announcement m-1">
+			<div className="card-body hstack gap-2">
+				<span className="flex-grow-1 line-clamp-1">
+					{currentAnnouncement.text}
+				</span>
+				<span className="flex-shrink-0">
+					{currentAnnouncement.pause ? "paused" : `until ${timeFormat.format(currentAnnouncement.endTime)}`}
+				</span>
+			</div>
+		</div>
+	</>
+}
+
+function Pools({ pools, bank, showBin }: { pools: AnnPools, bank: AnnBank, showBin: boolean }) {
+	return <div className="vstack w-50">
+		<h2>Announcement Pools</h2>
+		<Button className="d-inline" onClick={sendToF("addPool", {})}><PlusLg /> Add Pool</Button>
+		<div className="pools overflow-auto vstack gap-3 p-2">
+			{Object.entries(pools).map(([pid, pool]) => {
+				if (pool === undefined) return <h3 key={pid}>Error: Corresponding Pool does not exist for pool id {pid}</h3>
+				const contents = pool.announcements.map(aid => bank![aid.id]);
+				return <AnnPoolComp id={pid} key={pid} pool={pool} contents={contents} />
+			})}
+			{showBin && <div className="trash"><Trash className="queue-trash" /></div>}
+		</div>
+	</div>
+}
+
+function BarIframe() {
+	return <iframe src="/bundles/wasd/graphics/bar.graphic.html" height="70" width="1920" className="sticky-top" />;
 }
 
 const defaultPrelude = () => ({ lastCA: null, prelude: [], length: 0 });
@@ -60,47 +124,30 @@ export function AnnouncementsPanel() {
 
 
 	function onBeforeDragStart(start: DragStart) {
+		// Register drag start and show bin if dragging out of queue
 		setHover({ ...hv, dragging: true, showBin: start.source.droppableId === "queue" });
 	}
 
 	function onDragEnd(result: DropResult) {
+		// Reset bin + prelude
 		setHover({ ...hv, dragging: false, showBin: false });
 		setPrelude(defaultPrelude());
 
+		// Trigger correct effect of the move
 		const { source, destination } = result;
 		if (!destination) return;
-
-		if (source.droppableId !== "queue" && destination.droppableId !== "queue") {
-			if (source.index === destination.index) return;
-			const da = pools![destination.droppableId].announcements;
-			const ind = source.droppableId === destination.droppableId && source.index < destination.index ? destination.index + 1 : destination.index;
-			sendTo("movePool", {
-				aref: klona(pools![source.droppableId].announcements[source.index]),
-				oldpid: source.droppableId,
-				newpid: destination.droppableId,
-				before: klona(destination.index === da.length ? null : da[ind])
-			})
-		} else if (source.droppableId === "queue" && destination.droppableId === "queue") sendTo("reorderQueue", {
-			aref: queue!.announcements[source.index - prelude.prelude.length],
-			before: queue!.announcements[destination.index - prelude.prelude.length]
-		})
-		else if (source.droppableId !== "queue" && destination.droppableId === "queue") sendTo("enqueue", {
-			aid: pools![source.droppableId].announcements[source.index].id,
-			before: queue!.announcements[destination.index - prelude.prelude.length]
-		})
-		else { // (source.droppableId === "queue" && destination.droppableId !== "queue")
-			sendTo("dequeue", { aref: queue!.announcements[source.index - prelude.prelude.length] })
-		}
+		sendDragAndDropMove(source, destination, queue!, pools!, prelude.prelude.length);
 	}
 
+	// Fetch current announcement object from announcement bank
 	const currentAnn = bank && currentAnnouncement && currentAnnouncement.annID ? bank[currentAnnouncement.annID] : undefined;
 
 	const queueContents = queue!.announcements.map(aid => bank![aid.id]);
 	const queuePreludeAnns = prelude.prelude.map(aid => bank![aid.id]);
 	return (
 		<div className="vstack" style={{ height: "100vh" }}>
-			<iframe src="/bundles/wasd/graphics/bar.graphic.html" height="70" width="100%" className="sticky-top" style={{ maxWidth: 1920, margin: "auto" }} />
 			<div className='container-xxl h-0 flex-grow-1'>
+				<BarIframe />
 				<DragDropContext onDragEnd={onDragEnd} onBeforeDragStart={onBeforeDragStart}>
 					<div className='d-flex gap-3 h-100'>
 						<div className="w-50 overflow-auto sticky-top">
@@ -108,35 +155,14 @@ export function AnnouncementsPanel() {
 								<InputGroup className="card-ctrls announcement float-end">
 									<Button variant="outline-primary" onClick={() => currentAnnouncement.pause = !currentAnnouncement.pause}>{currentAnnouncement.pause ? <Play /> : <Pause />}</Button>
 								</InputGroup>
-								<h3>Current</h3>
-								<div className="card announcement m-1">
-									<div className="card-body hstack gap-2">
-										<span className="flex-grow-1 line-clamp-1">
-											{currentAnn.text}
-										</span>
-										<span className="flex-shrink-0">
-											{currentAnnouncement.pause ? "paused" : `until ${timeFormat.format(currentAnnouncement.endTime)}`}
-										</span>
-									</div>
-								</div>
+								<CurrentRun currentAnnouncement={currentAnnouncement} />
 							</div>)}
 							{queue && (<div className="p-2" onMouseEnter={() => setHover({ ...hv, hoverQueue: true })} onMouseLeave={() => setHover({ ...hv, hoverQueue: false })}>
 								<h3>Queue</h3>
 								<AnnPoolComp id="queue" pool={queue} contents={queueContents} preludeInfo={prelude} prelude={queuePreludeAnns} />
 							</div>)}
 						</div>
-						<div className="vstack w-50">
-							<h2>Announcement Pools</h2>
-							<Button className="d-inline" onClick={sendToF("addPool", {})}><PlusLg /> Add Pool</Button>
-							<div className="pools overflow-auto vstack gap-3 p-2">
-								{Object.entries(pools).map(([pid, pool]) => {
-									if (pool === undefined) return <h3 key={pid}>Error: Corresponding Pool does not exist for pool id {pid}</h3>
-									const contents = pool.announcements.map(aid => bank![aid.id]);
-									return <AnnPoolComp id={pid} key={pid} pool={pool} contents={contents} />
-								})}
-								{hv.showBin && <div className="trash"><Trash className="queue-trash" /></div>}
-							</div>
-						</div>
+						<Pools pools={pools} bank={bank!} showBin={hv.showBin} />
 					</div>
 				</DragDropContext >
 			</div>
