8dfbcb788e41d19dc06c838357d2b8b2787b2f81
ericthelemur <owenconnors1@gmail.com>
2024-01-07 15:21:17 +0000

    Adapt to common repo structure

--- src/graphics/overlay/overlay.graphic.tsx
+++ src/graphics/overlay/overlay.graphic.tsx
@@ -1,11 +1,12 @@
-import "../uwcs-bootstrap.css";
+import 'wasd-common/shared/uwcs-bootstrap.css';
 
 import { useEffect, useState } from 'react';
 import ReactCSSTransitionReplace from 'react-css-transition-replace';
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 import { Textfit } from 'react-textfit';
 import { useReplicant } from 'use-nodecg';
-import Overlay16x9 from "./single/16-9";
+
+import Overlay16x9 from './single/16-9';
 
 function Overlay() {
     return <Overlay16x9 />
