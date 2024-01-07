56ca3e7d8cfd7a527e3e986f52a7f192f7df4cbf
ericthelemur <owenconnors1@gmail.com>
2024-01-07 13:35:29 +0000

    Update for workspace system

--- src/dashboard/people-control/people.dashboard.tsx
+++ src/dashboard/people-control/people.dashboard.tsx
@@ -1,16 +1,16 @@
-import '../uwcs-bootstrap.css';
-import "./people.scss"
+import 'wasd-common/shared/uwcs-bootstrap.css';
+import './people.scss';
 
-import { createRoot } from 'react-dom/client';
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import Button from "react-bootstrap/Button";
+import { RecordFill, Wifi } from 'react-bootstrap-icons';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Badge from "react-bootstrap/Badge";
-import Stack from "react-bootstrap/Stack";
-import { RecordFill, Wifi } from "react-bootstrap-icons";
-import { useReplicant } from "use-nodecg";
-import { PreviewScene, ProgramScene, SceneList, ObsStatus, ConnStatus, Login } from 'types/schemas';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Stack from 'react-bootstrap/Stack';
+import { createRoot } from 'react-dom/client';
+import { ConnStatus, Login, ObsStatus, PreviewScene, ProgramScene, SceneList } from 'types/schemas';
+import { useReplicant } from 'use-nodecg';
 
 function Person() {
 	return <></>
