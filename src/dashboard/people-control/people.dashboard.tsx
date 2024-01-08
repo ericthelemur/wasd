9e09bbe3b3fd04076ad3ec6430e6f9e4a97621e3
ericthelemur <owenconnors1@gmail.com>
2024-01-08 13:24:16 +0000

    Use modal to edit person

--- src/dashboard/people-control/people.dashboard.tsx
+++ src/dashboard/people-control/people.dashboard.tsx
@@ -3,12 +3,14 @@ import './people.scss';
 
 import { useState } from 'react';
 import { DragDropContext, DraggableProvided, DroppableProvided } from 'react-beautiful-dnd';
-import { At, DistributeVertical, GripVertical, RecordFill, Wifi } from 'react-bootstrap-icons';
-import Accordion from 'react-bootstrap/Accordion';
-import Badge from 'react-bootstrap/Badge';
+import {
+    At, DistributeVertical, GripVertical, PenFill, RecordFill, Wifi
+} from 'react-bootstrap-icons';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Modal from 'react-bootstrap/Modal';
 import Stack from 'react-bootstrap/Stack';
 import { createRoot } from 'react-dom/client';
 import { Icon, People, Person, Social, Socials } from 'types/schemas';
@@ -43,36 +45,64 @@ function SocialComp({ soc, provided }: { soc: Social, provided: DraggableProvide
 	const [socials,] = useReplicant<Socials>("socials", {});
 	const icon = socials![social];
 
-	return <Stack direction="horizontal" gap={1} ref={provided.innerRef} {...provided.draggableProps}>
-		<div {...provided.dragHandleProps}>
+	return <InputGroup className="m-1" ref={provided.innerRef} {...provided.draggableProps}>
+		<div className="btn btn-outline-secondary" {...provided.dragHandleProps}>
 			<GripVertical />
 		</div>
-		<SocialIcon icon={icon} />
-		<Editable text={name} setText={(v) => soc.name = v} type="multi" />
-	</Stack>
+
+		<div className="btn btn-outline-secondary" >
+			<SocialIcon icon={icon} />
+		</div>
+		<Editable className='msg-text' textClasses="input-group-text" text={name} setText={(v) => soc.name = v} type="multi" container={false} />
+	</InputGroup>
 }
 
-function PersonComp({ person, provided, onHandle }: { person: Person, provided: DraggableProvided, onHandle: () => {} }) {
+interface PersonProps {
+	person: Person;
+	provided: DraggableProvided;
+	onHandle: () => {};
+	setEditPerson: (p: Person) => {};
+}
+
+function PersonComp({ person, provided, onHandle, setEditPerson }: PersonProps) {
 	const { name, socials } = person;
-	return <Accordion.Item ref={provided.innerRef} {...provided.draggableProps} eventKey={name}>
-		<Accordion.Header>
-			<InsertHandle onClick={onHandle} />
-			<Stack direction="horizontal" className="gap-1 person">
-				<div {...provided.dragHandleProps}>
-					<GripVertical />
-				</div>
-				<Editable text={name} setText={(v) => person.name = v} type="multi" />
-			</Stack>
-		</Accordion.Header>
-		<Accordion.Body>
-			<DnDTransitionsList id={"socials::" + name} type={"socials::" + name}
-				ids={(socials ?? []).map(s => name + "::" + s.name)}
-				data={socials ?? []}
-				content={(id, index, s, provided) => {
-					return <SocialComp soc={s} provided={provided} />
-				}} />
-		</Accordion.Body>
-	</Accordion.Item>
+	return <Card ref={provided.innerRef} {...provided.draggableProps} className="m-1">
+		<InsertHandle onClick={onHandle} />
+		<Stack direction="horizontal" className="gap-1 person h-4">
+			<div {...provided.dragHandleProps} className="px-1">
+				<GripVertical />
+			</div>
+			<div className="flex-grow-1 editable lh-1" onClick={() => setEditPerson(person)}>
+				{name} <PenFill className="icon" />
+			</div>
+		</Stack>
+	</Card>
+}
+
+function EditModal({ person, setEditPerson }: { person: Person, setEditPerson: (p: Person | null) => {} }) {
+	return <Modal show={true} fullscreen="md-down" onHide={() => setEditPerson(null)}>
+		<Modal.Header closeButton className="h4">Edit {person.name}</Modal.Header>
+		<Modal.Body>
+			<Form>
+				<Form.Group className="hstack gap-1">
+					<Form.Label className="m-0 h6">Name: </Form.Label>
+					<InputGroup>
+						<Editable text={person.name} setText={(v) => person.name = v} type="single" />
+					</InputGroup>
+				</Form.Group>
+				<hr className="my-2" />
+				<Form.Group>
+					<Form.Label className="h4 m-0">Socials:</Form.Label>
+					<DnDTransitionsList id={"socials::" + name} type={"socials::" + name}
+						ids={(person.socials ?? []).map(s => name + "::" + s.name)}
+						data={person.socials ?? []}
+						content={(id, index, s, provided) => {
+							return <SocialComp soc={s} provided={provided} />
+						}} />
+				</Form.Group>
+			</Form>
+		</Modal.Body>
+	</Modal>
 }
 
 function InsertHandle(props: { onClick: () => void }) {
@@ -85,15 +115,19 @@ function InsertHandle(props: { onClick: () => void }) {
 
 export function PeoplePanel() {
 	const [people,] = useReplicant<People>("people", []);
+	const [editPerson, setEditPerson] = useState<Person | null>(null);
 	return <DragDropContext onDragEnd={() => { }}>
-		<Accordion className="m-2">
+		<Stack className="m-2">
 			<DnDTransitionsList id={"people"} type="people"
 				ids={(people ?? []).map(p => p.name)}
 				data={people ?? []}
 				content={(id, index, p, provided) => {
-					return <PersonComp person={p} provided={provided} onHandle={() => people?.splice(index, 0, { name: "New", socials: [] })} />
+					return <PersonComp person={p} provided={provided}
+						setEditPerson={setEditPerson}
+						onHandle={() => people?.splice(index, 0, { name: "New", socials: [] })} />
 				}} />
-		</Accordion>
+		</Stack>
+		{editPerson && <EditModal person={editPerson} setEditPerson={setEditPerson} />}
 	</DragDropContext>
 }
 
