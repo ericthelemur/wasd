6be993b7835d9f366109b38aad8d486353136e35
ericthelemur <owenconnors1@gmail.com>
2024-01-20 21:26:41 +0000

    Add run estimate

--- src/graphics/overlay/components/game.tsx
+++ src/graphics/overlay/components/game.tsx
@@ -24,11 +24,15 @@ export function Game() {
 
 export function TimerComp() {
     const [timer,] = useReplicant<Timer | undefined>("timer", undefined, { namespace: "nodecg-speedcontrol" })
+    const [activeRun,] = useReplicant<RunData | undefined>("runDataActiveRun", undefined, { namespace: "nodecg-speedcontrol" })
     if (!timer) return <></>
     return <div className="h1 m-3 text-center tabnum" style={{ fontSize: 72 }}>
         {timer?.time}
         <span className="ms-auto tabnum" style={{ fontSize: 26 }}>
             .{String(Math.trunc(timer?.milliseconds / 10) % 100).padStart(2, "0")}
         </span>
+        <div style={{ fontSize: "0.4em" }}>
+            Estimate: {activeRun?.estimate}
+        </div>
     </div >;
 }
