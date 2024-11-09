9b9567ae7c4a3e4eb38a1eab911f571093bb91d4
ericthelemur <owenconnors1@gmail.com>
2024-11-09 23:59:21 +0000

    Prepare for merge

--- src/types/schemas/index.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-// @ts-ignore
-export * from './configschema';
-// @ts-ignore
-export * from './people';
-// @ts-ignore
-export * from './peopleBank';
-// @ts-ignore
-export * from './socials';
