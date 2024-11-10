2376085662120e8dbe72150af9e2b924aa36a0e7
ericthelemur <owenconnors1@gmail.com>
2024-11-10 01:14:58 +0000

    Generate new types

---
+++ src/types/schema/index.d.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+export * from './people';
+// @ts-ignore
+export * from './ticker';
+// @ts-ignore
+export * from './wasd';
+// @ts-ignore
+export * from './configschema';
---
+++ src/types/schema/wasd/index.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable */
+// @ts-ignore
+export * from './countdown';
+// @ts-ignore
+export * from './customBreakText';
+// @ts-ignore
+export * from './sceneData';
