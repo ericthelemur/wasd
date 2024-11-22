37c2c1c2b9c04b595f1f96be70a5fa61c394f767
Owen Connors <owenconnors1@gmail.com>
2024-11-22 00:28:02 +0000

    Test multirole auth

--- src/mixer/extension/index.extension.ts
+++ src/mixer/extension/index.extension.ts
@@ -1,7 +1,19 @@
+import { getNodeCG } from '../../common/utils';
 import { storeX32 } from './mixer/utils';
 
-const x32 = require("./X32");
+const x32 = require("./mixer/X32");
 const x32u = new x32.X32Utility();
 storeX32(x32u);
 
-const list = require("./listeners");
+const list = require("./mixer/listeners");
+
+const nodecg = getNodeCG();
+const router = nodecg.Router();
+
+router.use((nodecg.util as any).authCheckRole("external"));
+
+router.get('/customroute', (req, res) => {
+    res.send('OK!');
+});
+
+nodecg.mount('/external', router);
