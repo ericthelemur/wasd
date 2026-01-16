97b98ad585807df02cc08e2a1bf606d6f926a47c
Owen Connors <owenconnors1@gmail.com>
2026-01-16 14:48:34 +0000

    Add script to remove abort from loupedeck-core

---
+++ removeAbort.ps1
@@ -0,0 +1,6 @@
+# NodeCG Speedcontrol uses Livesplit-Core to time things
+# Livesplit core has a really annoying line: process.on("unhandledRejection", abort), which will make the program crash and flood the console on any unhandled promise
+# I don't know of a good way to remove this line otherwise. This will just remove the line from the file
+# I recommend running it on a fresh install and on updating nodecg-speedcontrol
+
+(Get-Content .\bundles\nodecg-speedcontrol\node_modules\livesplit-core\livesplit_core.js).Replace('process["on"]("unhandledRejection",abort);', '') | Set-Content .\bundles\nodecg-speedcontrol\node_modules\livesplit-core\livesplit_core.js
